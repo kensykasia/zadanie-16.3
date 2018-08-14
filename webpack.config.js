0d0496379c982ee76a3e1dee1685b1e58c4688fc
Katarzyna Kensy <zbierek@gmail.com>
2018-08-14 12:23:32 +0200

    Add .gitignore and change webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 //webpack.config.js
 module.exports = {
-entry: './src/app.js',
-output: {
-path: path.resolve(__dirname, 'build'),
-filename: 'app.bundle.js'
-}
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'app.bundle.js'
+    }
 };
